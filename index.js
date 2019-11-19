872572d95a02e1d0f28a5f3136872d3932567c12
MuriloPetruci <murilopetruci@hotmail.com>
2019-11-19 10:20:56 -0300

    rotas

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+let numberOfRequests = 0;
+const projects = [];
+
+/*criado middleware que será utilizado em todas as rotas que recebem o ID do projeto nos parametros da url que verifica se o projeto com aquele ID existe se não existir retonar um erro caso contrario permite que a requisição continue normalmente*/
+// criado middleware global chama todas as requisições que imprime (console.log) uma contagem de quantas requisições
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id === id);
+
+  if (!project) {
+    return res.status(400).json({ error: 'Project not found' });
+  }
+
+  return next();
+}
+
+function logRequests(req, res, next) {
+  numberOfRequests++;
+
+  console.log(`Número de requisições: ${numberOfRequests}`);
+
+  return next();
+}
+
+server.use(logRequests);
+
+//retorna novos projetos
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id === id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+// deleta o porjeto associado ao ID presente nos parametros da rota
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id === id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
+
+//adicione uma nova tarefa no projeto escolhido via ID
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id === id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});;
+
+server.listen(3000);
